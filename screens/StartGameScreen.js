3f0f867c3df804a841fd20e73166b0d06b615810
Andres Fernando <andres.fernando08@gmail.com>
2020-03-28 22:12:41 -0500

    aprendiendo a mostrar alertas y trucos para cambiar entre pantallas en la app

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,85 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Button, Keyboard, Alert } from 'react-native'
+
+import Card from '../components/Card'
+import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
+
+import Colors from '../constants/colors'
+
 
 const StartGameScreen = props => {
+    const [enteredValue, setEnteredValue] = useState('')
+    const [confirmed, setConfirmed] = useState(false)
+    const [selectedNumber, setSelectedNumber] = useState()
+
+    const numerInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('')
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue)
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number', 'Number has to be a number between 1 to 99', [{ text: 'OK', style: 'destructive', onPress: resetInputHandler }])
+            return
+        }
+        setConfirmed(true)
+        setSelectedNumber(parseInt(enteredValue))
+        setEnteredValue('')
+        Keyboard.dismiss()
+    }
+
+
+    let confirmedOutput
+    if (confirmed) {
+        confirmedOutput = (
+            <Card style={styles.summary}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)} />
+            </Card>
+        )
+    }
 
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game</Text>
-            <View style={styles.inputContainer}>
-                <Text>Select a number:</Text>
-                <TextInput placeholder='Number' />
-                <View style={styles.buttons}>
-                    <Button title='Reset' onPress={() => {}} />
-                    <Button title='Confirm' onPress={() => {}} />
-                </View>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss()
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a number:</Text>
+                    <Input 
+                        style={styles.input} 
+                        placeholder='Number'
+                        blurOnSubmit 
+                        autoCapitalize='none' 
+                        autoCorrect={false} 
+                        keyboardType='numeric' 
+                        maxLength={2}
+                        onChangeText={numerInputHandler}
+                        value={enteredValue} />
+                    <View style={styles.buttons}>
+                        <View style={styles.btn}>
+                            <Button 
+                                color={Colors.accent} 
+                                title='Reset' onPress={resetInputHandler} />
+                        </View>
+                        <View style={styles.btn}>
+                            <Button 
+                                color={Colors.primary} 
+                                title='Confirm' onPress={confirmInputHandler} />
+                        </View>
+                    </View>
+                </Card>
+                {confirmedOutput}
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -32,21 +97,24 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 6,
-        padding: 10,
-        borderRadius: 10
+        alignItems: 'center'
+    },
+    input: {
+        width: '40%',
+        textAlign: 'center'
     },
     buttons: {
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
-        paddingHorizontal: 15
+        paddingHorizontal: 10
+    },
+    btn: {
+        width: '40%'
+    },
+    summary: {
+        alignItems: 'center',
+        marginTop: 30
     }
 })
 
