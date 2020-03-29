467fe1c0b3ef988e24db9d0fb9c4e1ddb59df1fb
Andres Fernando <andres.fernando08@gmail.com>
2020-03-28 22:10:23 -0500

    se creo un objeto constante para guardar colores,

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import Colors from '../constants/colors'
+
 const Header = props => (
     <View style={styles.header}>
         <Text style={styles.headerTitle}>{props.title}</Text>
@@ -12,12 +14,12 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#768BFF',
+        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center'
     },
     headerTitle: {
-        color: '#EBEBEB',
+        color: Colors.primaryText,
         fontSize: 18
     }
 })
---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+    primary: '#748EFF',
+    accent: '#65C3FF',
+    primaryText: '#EBEBEB',
+    white: '#B9B8BA'
+}
