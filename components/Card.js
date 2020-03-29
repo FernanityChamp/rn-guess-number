3ac4afe349ebdc26f7747d9078ba80c24dd8bd57
Andres Fernando <andres.fernando08@gmail.com>
2020-03-28 22:10:57 -0500

    se crearon componentes necesarios para mostrar el numero del usuario

---
+++ components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+
+const Card = props => (
+    <View style={{...styles.card, ...props.style}}>{props.children}</View>
+)
+
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 6,
+        padding: 10,
+        borderRadius: 10
+    }
+})
+
+export default Card
